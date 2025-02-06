49620c04a3488ceb975f9cd37aa8585eccc8c132
Fonyuy-pounds <fonyuypounds@gmail.com>
2025-02-07 08:59:18 +0900

    Added twoSum solutions using hash maps

---
+++ Road_to_pro-master/1. day1/solution.py
@@ -0,0 +1,14 @@
+def twoSum(num,target):
+    num_map={} 
+    for index, num in enumerate(num):
+        complement = target - num
+        if complement in num_map:
+            return[num_map[complement],index]
+        num_map[num] = index
+    return[]
+num=[2,7,5,11]
+num =list(map(int, input("enter the array of numbers (separated by spaces:) ").split()))
+target=int(input("enter the target valu]e:"))
+#target=9
+result =twoSum(num,target)
+print(result)
